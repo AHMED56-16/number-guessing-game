6713936c67c7ed679b5b767509a9b815e9191410
AHMED56-16 <muhammadahmedraza445@gmail.com>
2024-11-21 00:08:02 +0500

    Guess the number

---
+++ app.js
@@ -0,0 +1,51 @@
+let guessedNumber = document.querySelector(".guessedNumber");
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let high_score = 0;
+
+function checkNumber() {
+    let userInput = Number(document.querySelector(".guessedNumber").value);
+    if (isNaN(userInput) || userInput < 1 || userInput > 20) {
+        alert("Please enter a valid number between 1 and 20.");
+        return;
+    }
+    
+    if (userInput === randomNumber) {
+        alert("Congratulations! You guessed the correct number.");
+        if (score > high_score) {
+            high_score = score;
+        }
+        document.querySelector(".message").innerHTML = "Start guessing.....";
+        document.querySelector(".score").textContent = "Score: 20";
+        document.querySelector(".high-score").textContent = "High Score: " + high_score;
+        
+        // Clear input field after winning
+        document.querySelector(".guessedNumber").value = "";
+        
+        // Generate a new random number for the next round
+        randomNumber = Math.floor(Math.random() * 20) + 1;
+        
+        // Reset score back to 20
+        score = 20;
+    }
+    else if (userInput < randomNumber) {
+        document.querySelector(".message").innerHTML = "Guess higher number.";
+        score--;
+        document.querySelector(".score").textContent = "Score: " + score;
+    }
+    else {
+        document.querySelector(".message").innerHTML = "Guess lower number.";
+        score--;
+        document.querySelector(".score").textContent = "Score: " + score;
+    }
+}
+
+function again() {
+    high_score=0;
+    document.querySelector(".message").innerHTML = "Start guessing.....";
+    document.querySelector(".score").textContent = "Score: 20";
+    document.querySelector(".high-score").textContent = "High Score: " + high_score; // keep the high score intact
+    document.querySelector(".guessedNumber").value = "";  
+    randomNumber = Math.floor(Math.random() * 20) + 1;
+    score = 20; // Reset score back to 20
+}
